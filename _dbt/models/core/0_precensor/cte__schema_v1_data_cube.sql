9a2bb0c887c753e5bee592172db5b8c00331d9ee
rl627 <rl627@drexel.edu>
2025-09-16 13:56:07 -0400

    removed legacy cte dbt models (schema v1/v2 stuff) #12

--- _dbt/models/core/0_precensor/cte__schema_v1_data_cube.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-   * ,
-   '' AS value_iteration
-FROM read_parquet('//files.drexel.edu/colleges/SOPH/Shared/UHC/Projects/Wellcome_Trust/Data Methods Core/Dashboards/dbt/salurbal-dbt-server/sources/_seeds/data_v1.parquet')
-{{ limit_data_in_dev() }}
--- _dbt/models/core/0_precensor/cte__schema_v1_metadata_cube.sql
+++
@@ -1,3 +0,0 @@
-SELECT 
-* 
-FROM read_parquet('//files.drexel.edu/colleges/SOPH/Shared/UHC/Projects/Wellcome_Trust/Data Methods Core/Dashboards/dbt/salurbal-dbt-server/sources/_seeds/metadata_v1.parquet')
